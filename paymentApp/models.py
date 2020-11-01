f2f181ed9aab114ef93832f922a00eb022d885f1
Misiker Degafe <mskrdgafie@gmail.com>
2020-11-01 19:25:50 +0300

    yenepay model added.

--- paymentApp/models.py
+++ paymentApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class YenePay(models.Model):
+    total_amount = models.CharField(max_length=100)
+    buyer_id= models.CharField(max_length=500)
+    marchant_order_id= models.CharField(max_length=500)
+    marchant_id= models.CharField(max_length=500)
+    merchant_code= models.CharField(max_length=500)
+    transaction_code= models.CharField(max_length=500)
+    transaction_id= models.CharField(max_length=500)
+    status= models.CharField(max_length=100)
+    currency= models.CharField(max_length=100)
+    signature= models.CharField(max_length=1000)
